e73f5474cd16a5bc39a94f02f5ff7c2fad9c319c
MARWANANO32 <marwanano32@gmail.com>
2023-11-29 00:00:13 +0200

    MARWAN

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # Author - Godswill Kalu
 
-def remove_char_at(str, n):
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
