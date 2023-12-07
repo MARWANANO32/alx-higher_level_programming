cbfd98db09761f6744cb8eec8e55cba42e54859b
MARWANANO32 <marwanano32@gmail.com>
2023-12-07 02:43:21 +0200

    MARWAN

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
+
 def search_replace(my_list, search, replace):
     """
-     function that replaces all occurrences 
-     of an element by another in a new list.
+    A function that replaces all occurrences
+    of an element by another in a new list
     """
     new_list = []
     for element in my_list:
