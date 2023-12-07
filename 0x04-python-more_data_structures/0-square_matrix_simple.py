3a9edcd7c25d7447d49d6e41aa09ea3453dcb06e
MARWANANO32 <marwanano32@gmail.com>
2023-12-07 02:30:32 +0200

    MARWAN

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,6 +6,6 @@ def square_matrix_simple(matrix=[]):
     """
     new_matrix = []
     for i in matrix:
-        result = list(map(lambda x : x*x, i))
+        result = list(map(lambda x: x*x, i))
         new_matrix.append(result)
     return new_matrix
