4609c17e8a85728e9401864271d5e50af00eead5
MARWANANO32 <marwanano32@gmail.com>
2024-01-05 02:38:38 +0200

    MARWAN

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" Define the Rectangle class"""
+class Rectangle:
+    """represent the rectangle."""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" defines a rectangle """
+class Rectangle:
+    """represent the rectangle"""
+    def __init__(self, width = 0, height = 0):
+        """initialize the recangle"""
+        self.width = width
+        self.height = height
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    def height(self):
+        return self.__height
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+         if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
