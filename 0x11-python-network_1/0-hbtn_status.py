ae3d75faab694e4a624110c297fa17a6496e9d99
MARWANANO32 <marwanano32@gmail.com>
2024-04-01 20:55:10 +0200

    MARWAN MOSTAFA

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,7 +14,3 @@ if __name__ == '__main__':
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
-        # If you want to print the headers of the response, you can do it like this:
-        print("\t- headers:")
-        for header in res.headers.items():
-            print("\t\t- {}: {}".format(header[0], header[1]))
